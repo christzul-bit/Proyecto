028227efde7df623f864adb73b875893830394ff
chris <chris@LAPTOPCHRIS>
2026-03-07 22:34:22 -0600

    Menu

--- Proyecto/Program.cs
+++ Proyecto/Program.cs
@@ -1,5 +1,10 @@
 ﻿//Inicio, creacion del menu
 int opcion;
+int contenido;
+int duracion;
+int clasificacion;
+double emicion;
+int nvlProduccion;
 do
 {
     Console.WriteLine("Regulación de programas semanales. \n" +
@@ -13,6 +18,33 @@ do
     switch (opcion)
     {
         case 1:
+            Console.WriteLine("Ingrese el tipo de contenido: \n" +
+                "1)Pelicula \n" +
+                "2)Serie \n" +
+                "3)Documental \n" +
+                "4)Evento en vivo_");
+            contenido = int.Parse(Console.ReadLine());
+            Console.WriteLine("Ingrese la duración en minutos_");
+            duracion = int.Parse(Console.ReadLine());
+            Console.WriteLine("Ingrese la clasificación: \n" +
+                "1)Todo publico \n" +
+                "2)+13 \n" +
+                "3)+18_");
+            clasificacion = int.Parse(Console.ReadLine());
+            Console.WriteLine("Ingrese la hora en la que se emitira el programa_");
+            emicion = double.Parse(Console.ReadLine());
+            Console.WriteLine("Ingrese el nivel de producción: \n" +
+                "1)Bajo \n" +
+                "2)Medio \n" +
+                "3)Alto_");
+            nvlProduccion = int.Parse(Console.ReadLine());
+            if(contenido > 0 && contenido < 5 && duracion > 0 && clasificacion > 0 && clasificacion < 4 && emicion > 0 && emicion < 24 &&  nvlProduccion > 0 && nvlProduccion < 4)
+            {
+                Console.WriteLine("Provando verificacion");
+            }else
+            {
+                Console.WriteLine("Una o más respuestas son invalidas, verifique nuevamente");
+            }
             break;
         case 2:
             break;
@@ -24,4 +56,7 @@ do
             break;
         default: break;
     }
+    Console.WriteLine("Precione cualquier tecla para continuar");
+    Console.ReadLine();
+    Console.Clear();
 } while (opcion != 5);
