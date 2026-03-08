e6193ec90a4b4522f8e468a58bade806a9b2343d
chris <chris@LAPTOPCHRIS>
2026-03-07 22:17:33 -0600

    Agregar archivos de proyecto.

---
+++ Proyecto/Program.cs
@@ -0,0 +1,27 @@
+﻿//Inicio, creacion del menu
+int opcion;
+do
+{
+    Console.WriteLine("Regulación de programas semanales. \n" +
+        "Seleccione una opción: \n" +
+        "1)Evaluar nuevo contenido \n" +
+        "2)Mostrar las reglas del sistema \n" +
+        "3)Mostrar estadísticas de la seción \n" +
+        "4)Reiniciar valores \n" +
+        "5)Salir_");
+    opcion = int.Parse(Console.ReadLine());
+    switch (opcion)
+    {
+        case 1:
+            break;
+        case 2:
+            break;
+        case 3:
+            break;
+        case 4:
+            break;
+        case 5:
+            break;
+        default: break;
+    }
+} while (opcion != 5);
